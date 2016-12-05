816f7bd9de57fd7066410f45f4f2e5495e991a50
Tom McClelland <tom@mcclelland.me>
2016-12-05 14:24:48 -0500

    staging files for collin

--- app/Http/Controllers/api/ShoppingListController.js
+++ app/Http/Controllers/api/ShoppingListController.js
@@ -1,9 +1,22 @@
 'use strict'
 
+const ShoppingList = use('App/Model/ShoppingList')
+const Cooker = use('App/Model/Cooker')
+
 class ShoppingListController {
 
   * index(request, response) {
-    //
+
+    const userId = request.input('id')
+    let cooker = yield Cooker.findBy('user_id', userId)
+
+    // response.json(cooker.shoppingLists())
+    // response.json(yield request.auth.getUser())
+
+    const shoppingList = yield ShoppingList.findBy('cooker_id', cooker.id)
+    // console.log(shoppingList.recipeIngredients())
+    return response.json({shoppingListId: shoppingList.id, ingredients: shoppingList})
+
   }
 
   * create(request, response) {
--- app/Model/ShoppingList.js
+++ app/Model/ShoppingList.js
@@ -3,21 +3,21 @@
 const Lucid = use('Lucid')
 
 class ShoppingList extends Lucid {
-    order () {
-        return this.belongsTo('App/Model/Order')
-    }
-
-    cooker () {
-        return this.belongsTo('App/Model/Cooker')
-    }
-
-    shoppingListIngredients () {
-      return this.hasMany('App/Model/IngredientShoppingList')
-    }
-
-    recipeIngredients () {
-    return this.hasManyThrough('App/Model/IngredientRecipe', 'App/Model/IngredientShoppingList')
-    }
+    // order () {
+    //     return this.belongsTo('App/Model/Order')
+    // }
+    //
+    // // cooker () {
+    // //     return this.belongsTo('App/Model/Cooker')
+    // // }
+    //
+    // shoppingListIngredients () {
+    //   return this.hasMany('App/Model/IngredientShoppingList')
+    // }
+    //
+    // recipeIngredients () {
+    // return this.hasManyThrough('App/Model/IngredientRecipe', 'App/Model/IngredientShoppingList')
+    // }
 
 }
 
