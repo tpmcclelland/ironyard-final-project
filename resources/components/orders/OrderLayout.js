82351fba816a8036895cd38d6ecb128b3bf52d1d
JoelBot <Joel.Lonneker@gmail.com>
2016-12-07 22:08:03 -0500

    added a centralized leftNav bar with cross site navication anchors.  Color and xs sizing changes made in cs as well as border sizing/color sitewide

--- resources/components/orders/OrderLayout.js
+++ resources/components/orders/OrderLayout.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import Header from '../common/Header'
 import OrderLeftNav from './OrderLeftNav'
+import MasterLeftNav from '../leftNav/masterLeftNav'
 
 class OrderLayout extends React.Component {
     constructor(props) {
@@ -12,7 +13,7 @@ class OrderLayout extends React.Component {
         return <div>
             <Header />
             <main className="row">
-                <OrderLeftNav />
+                <MasterLeftNav />
                 <div id="order" className="col-xs-12">
                     <section className="col-xs-12 main-section">
                         {this.props.children}
